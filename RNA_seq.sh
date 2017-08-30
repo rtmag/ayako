a40018e56e3668c2a00d794f218cf436d16158c8
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-08-30 12:46:52 +0800

    Update RNA_seq.sh

--- RNA_seq.sh
+++ RNA_seq.sh
@@ -4,3 +4,111 @@
 --sjdbGTFfile /root/ayako/ref/gencode.vM15.annotation.gtf --sjdbOverhang 100
 ##
 #
+#################################
+~/myPrograms/STAR/bin/Linux_x86_64/STAR --genomeDir /root/ayako/ref/mm10_star_sjdbO100/ \
+--readFilesCommand zcat \
+--runThreadN 40 \
+--readFilesIn /root/ayako/rna/Ctrl_CD41minus1_1.fastq.gz \
+/root/ayako/rna/Ctrl_CD41minus1_2.fastq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/ayako/bam/Ctrl_CD41minus1_
+
+~/myPrograms/STAR/bin/Linux_x86_64/STAR --genomeDir /root/ayako/ref/mm10_star_sjdbO100/ \
+--readFilesCommand zcat \
+--runThreadN 40 \
+--readFilesIn /root/ayako/rna/Ctrl_CD41minus2_1.fastq.gz \
+/root/ayako/rna/Ctrl_CD41minus2_2.fastq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/ayako/bam/Ctrl_CD41minus2_
+
+~/myPrograms/STAR/bin/Linux_x86_64/STAR --genomeDir /root/ayako/ref/mm10_star_sjdbO100/ \
+--readFilesCommand zcat \
+--runThreadN 40 \
+--readFilesIn /root/ayako/rna/Ctrl_CD41minus3_1.fastq.gz \
+/root/ayako/rna/Ctrl_CD41minus3_2.fastq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/ayako/bam/Ctrl_CD41minus3_
+#############################################
+~/myPrograms/STAR/bin/Linux_x86_64/STAR --genomeDir /root/ayako/ref/mm10_star_sjdbO100/ \
+--readFilesCommand zcat \
+--runThreadN 40 \
+--readFilesIn /root/ayako/rna/Ctrl_CD41plus1_1.fastq.gz \
+/root/ayako/rna/Ctrl_CD41plus1_2.fastq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/ayako/bam/Ctrl_CD41plus1_
+
+~/myPrograms/STAR/bin/Linux_x86_64/STAR --genomeDir /root/ayako/ref/mm10_star_sjdbO100/ \
+--readFilesCommand zcat \
+--runThreadN 40 \
+--readFilesIn /root/ayako/rna/Ctrl_CD41plus2_1.fastq.gz \
+/root/ayako/rna/Ctrl_CD41plus2_2.fastq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/ayako/bam/Ctrl_CD41plus2_
+
+~/myPrograms/STAR/bin/Linux_x86_64/STAR --genomeDir /root/ayako/ref/mm10_star_sjdbO100/ \
+--readFilesCommand zcat \
+--runThreadN 40 \
+--readFilesIn /root/ayako/rna/Ctrl_CD41plus3_1.fastq.gz \
+/root/ayako/rna/Ctrl_CD41plus3_2.fastq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/ayako/bam/Ctrl_CD41plus3_
+############################################
+~/myPrograms/STAR/bin/Linux_x86_64/STAR --genomeDir /root/ayako/ref/mm10_star_sjdbO100/ \
+--readFilesCommand zcat \
+--runThreadN 40 \
+--readFilesIn /root/ayako/rna/Ctrl_neg_4_1.fastq.gz \
+/root/ayako/rna/Ctrl_neg_4_2.fastq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/ayako/bam/Ctrl_neg_
+
+###############################################
+
+~/myPrograms/STAR/bin/Linux_x86_64/STAR --genomeDir /root/ayako/ref/mm10_star_sjdbO100/ \
+--readFilesCommand zcat \
+--runThreadN 40 \
+--readFilesIn /root/ayako/rna/Thpo_CD41minus1_1.fastq.gz \
+/root/ayako/rna/Thpo_CD41minus1_2.fastq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/ayako/bam/Thpo_CD41minus1_
+
+~/myPrograms/STAR/bin/Linux_x86_64/STAR --genomeDir /root/ayako/ref/mm10_star_sjdbO100/ \
+--readFilesCommand zcat \
+--runThreadN 40 \
+--readFilesIn /root/ayako/rna/Thpo_CD41minus2_1.fastq.gz \
+/root/ayako/rna/Thpo_CD41minus2_2.fastq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/ayako/bam/Thpo_CD41minus2_
+
+~/myPrograms/STAR/bin/Linux_x86_64/STAR --genomeDir /root/ayako/ref/mm10_star_sjdbO100/ \
+--readFilesCommand zcat \
+--runThreadN 40 \
+--readFilesIn /root/ayako/rna/Thpo_CD41minus3_1.fastq.gz \
+/root/ayako/rna/Thpo_CD41minus3_2.fastq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/ayako/bam/Thpo_CD41minus3_
+
+###############################################
+
+~/myPrograms/STAR/bin/Linux_x86_64/STAR --genomeDir /root/ayako/ref/mm10_star_sjdbO100/ \
+--readFilesCommand zcat \
+--runThreadN 40 \
+--readFilesIn /root/ayako/rna/Thpo_CD41plus1_1.fastq.gz \
+/root/ayako/rna/Thpo_CD41plus1_2.fastq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/ayako/bam/Thpo_CD41plus1_
+
+~/myPrograms/STAR/bin/Linux_x86_64/STAR --genomeDir /root/ayako/ref/mm10_star_sjdbO100/ \
+--readFilesCommand zcat \
+--runThreadN 40 \
+--readFilesIn /root/ayako/rna/Thpo_CD41plus2_1.fastq.gz \
+/root/ayako/rna/Thpo_CD41plus2_2.fastq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/ayako/bam/Thpo_CD41plus2_
+
+~/myPrograms/STAR/bin/Linux_x86_64/STAR --genomeDir /root/ayako/ref/mm10_star_sjdbO100/ \
+--readFilesCommand zcat \
+--runThreadN 40 \
+--readFilesIn /root/ayako/rna/Thpo_CD41plus3_1.fastq.gz \
+/root/ayako/rna/Thpo_CD41plus3_2.fastq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/ayako/bam/Thpo_CD41plus3_
