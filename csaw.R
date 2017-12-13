db955b1900ebe6f4a0f27a685d97999dd092b049
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-12-13 16:22:29 +0800

    Update csaw.R

--- csaw.R
+++ csaw.R
@@ -1,1 +1,66 @@
 
+bed_to_granges <- function(file){
+   df <- read.table(file,
+                    header=F,
+                    stringsAsFactors=F)
+ 
+   if(length(df) > 6){
+      df <- df[,-c(7:length(df))]
+   }
+ 
+   if(length(df)<3){
+      stop("File has less than 3 columns")
+   }
+ 
+   header <- c('chr','start','end','id','score','strand')
+   names(df) <- header[1:length(names(df))]
+ 
+   if('strand' %in% colnames(df)){
+      df$strand <- gsub(pattern="[^+-]+", replacement = '*', x = df$strand)
+   }
+ 
+   library("GenomicRanges")
+ 
+   if(length(df)==3){
+      gr <- with(df, GRanges(chr, IRanges(start, end)))
+   } else if (length(df)==4){
+      gr <- with(df, GRanges(chr, IRanges(start, end), id=id))
+   } else if (length(df)==5){
+      gr <- with(df, GRanges(chr, IRanges(start, end), id=id, score=score))
+   } else if (length(df)==6){
+      gr <- with(df, GRanges(chr, IRanges(start, end), id=id, score=score, strand=strand))
+   }
+   return(gr)
+}
+
+library(csaw)
+
+blacklist=bed_to_granges("/root/ayako/ayako_dejavu/mm10.blacklist.bed")
+param <- readParam(minq=10,discard=blacklist)
+
+
+bam.files <- c('/root/ayako/ayako_dejavu/bam/CD41+_untr_1_Aligned_rmdup.sortedByCoord.out.bam',
+              '/root/ayako/ayako_dejavu/bam/CD41+_untr_2_Aligned_rmdup.sortedByCoord.out.bam',
+              '/root/ayako/ayako_dejavu/bam/CD41+_untr_3_Aligned_rmdup.sortedByCoord.out.bam',
+              '/root/ayako/ayako_dejavu/bam/CD41+_tr_1_Aligned_rmdup.sortedByCoord.out.bam',
+              '/root/ayako/ayako_dejavu/bam/CD41+_tr_2_Aligned_rmdup.sortedByCoord.out.bam',
+              '/root/ayako/ayako_dejavu/bam/CD41-_tr_1_Aligned_rmdup.sortedByCoord.out.bam',
+              '/root/ayako/ayako_dejavu/bam/CD41-_tr_2_Aligned_rmdup.sortedByCoord.out.bam')
+
+param <- readParam(minq=10,pe='both')
+data <- windowCounts(bam.files, width=10, param=param)
+                     
+library(edgeR)
+keep <- aveLogCPM(asDGEList(data)) >= -1
+data <- data[keep,]
+                     
+binned <- windowCounts(bam.files, bin=TRUE, width=10000, param=param)
+data <- normOffsets(binned, se.out=data)
+                     
+y <- asDGEList(data)
+y <- estimateDisp(y, design)
+fit <- glmQLFit(y, design, robust=TRUE)
+results <- glmQLFTest(fit)
+                     
+merged <- mergeWindows(rowRanges(data), tol=1000L)
+tabcom <- combineTests(merged$id, results$table)
