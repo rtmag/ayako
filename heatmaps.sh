cb1ef16ed413d88abe66c02dc5d3a4923ede79e4
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-02-13 20:15:42 +0800

    Update heatmaps.sh

--- heatmaps.sh
+++ heatmaps.sh
@@ -19,7 +19,7 @@ computeMatrix reference-point \
 /root/ayako/ayako_dejavu/bw/CD41-_tr_2.bw \
 /root/ayako/ayako_dejavu/bw/CD41-_untr_1.bw \
 /root/ayako/ayako_dejavu/bw/CD41-_untr_2.bw \
--R /root/ayako/ayako_dejavu/diffbind/diffbind_deseq2/ATAC-Seq_merged_LRT_FDR5.bed --referencePoint center \
+-R /root/ayako/ayako_dejavu/diffbind/diffbind_deseq2/ATAC-Seq_merged_LRT_FDR5_.bed --referencePoint center \
 --sortRegions descend -bs 20 -a 1000 -b 1000 -p 40 -out /root/ayako/ayako_dejavu/heatmaps/ATAC-Seq_merged_LRT_FDR5.mat
 
 plotHeatmap --xAxisLabel "" --refPointLabel "ATAC Peak" --colorMap Blues -m /root/ayako/ayako_dejavu/heatmaps/ATAC-Seq_merged_LRT_FDR5.mat -out /root/ayako/ayako_dejavu/heatmaps/ATAC-Seq_merged_LRT_FDR5.pdf
@@ -36,7 +36,7 @@ computeMatrix reference-point \
 /root/ayako/ayako_dejavu/bw/CD41-_tr_2.bw \
 /root/ayako/ayako_dejavu/bw/CD41-_untr_1.bw \
 /root/ayako/ayako_dejavu/bw/CD41-_untr_2.bw \
--R /root/ayako/ayako_dejavu/diffbind/diffbind_deseq2/ATAC-Seq_merged_LRT_FDR1.bed --referencePoint center \
+-R /root/ayako/ayako_dejavu/diffbind/diffbind_deseq2/ATAC-Seq_merged_LRT_FDR1_.bed --referencePoint center \
 --sortRegions descend -bs 20 -a 1000 -b 1000 -p 40 -out /root/ayako/ayako_dejavu/heatmaps/ATAC-Seq_merged_LRT_FDR1.mat
 
 plotHeatmap --xAxisLabel "" --refPointLabel "ATAC Peak" --colorMap Blues -m /root/ayako/ayako_dejavu/heatmaps/ATAC-Seq_merged_LRT_FDR1.mat -out /root/ayako/ayako_dejavu/heatmaps/ATAC-Seq_merged_LRT_FDR1.pdf
