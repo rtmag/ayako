97dc9f5e51685161f4e315f20fba8c6f89af1d9b
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-03-27 04:17:23 +0800

    Update single_cell_RNA-Seq.sh

--- single_cell_RNA-Seq.sh
+++ single_cell_RNA-Seq.sh
@@ -1,6 +1,9 @@
 
 
+for file in /root/ayako/single_cell/giga_38e_cat/*; do
+    trim_galore --quality 20 --illumina -o /root/ayako/single_cell/giga_38e_cat_trim/ $file
+done
 
-trim_galore --illumina --paired -o /root/ayako/single_cell/giga_1ad_cat/ \
-/home/roberto/deepa/h3k9me3/C_H3_L001/C-H3_S6_L001_R1_001.fastq.gz \
-/home/roberto/deepa/h3k9me3/C_H3_L001/C-H3_S6_L001_R2_001.fastq.gz
+for file in /root/ayako/single_cell/giga_1ad_cat/*; do
+    trim_galore --quality 20 --illumina -o /root/ayako/single_cell/giga_1ad_cat_trim/ $file
+done
