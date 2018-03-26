2a4a5558a764a9a9fbb430c4356d0fe501d1bb94
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-03-27 03:38:45 +0800

    Create single_cell_RNA-Seq.sh

---
+++ single_cell_RNA-Seq.sh
@@ -0,0 +1,6 @@
+
+
+
+trim_galore --illumina --paired -o /root/ayako/single_cell/giga_1ad_cat/ \
+/home/roberto/deepa/h3k9me3/C_H3_L001/C-H3_S6_L001_R1_001.fastq.gz \
+/home/roberto/deepa/h3k9me3/C_H3_L001/C-H3_S6_L001_R2_001.fastq.gz
