b2976ec31f8ff9a2ba64720e99da7efaae516aa6
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-04-18 19:28:26 +0800

    Update bulk_rna_seq.R

--- bulk_rna_seq.R
+++ bulk_rna_seq.R
@@ -305,14 +305,25 @@ vsd = readRDS("vsd_bulk_ayako.rds")
 dLRT_res = readRDS("dLRT_res_bulk_ayako.rds")
 
 
-png("test2.png")
+postscript("test.ps",height=10,width=10,horizontal=F)
 sig_vsd = vsd[which(dLRT_res$padj<0.05),]
 colnames(sig_vsd) <- c("CD41-_Ctrl","CD41-_Ctrl","CD41-_Ctrl",
                       "CD41+_Ctrl","CD41+_Ctrl","CD41+_Ctrl",
                       "CD41-_Thpo","CD41-_Thpo","CD41-_Thpo",
                       "CD41+_Thpo","CD41+_Thpo","CD41+_Thpo")
-colors <- colorRampPalette( (brewer.pal(9, "RdBu")) )(20)
+colors <- rev(colorRampPalette( (brewer.pal(9, "RdBu")) )(20))
 heatmap.2(sig_vsd,col=colors,scale="row", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
-labRow = FALSE,xlab="", ylab="Genes",key.title="Gene expression",cexCol=.7)
+labRow = FALSE,xlab="", ylab="Genes",key.title="Gene expression",cexCol=.8)
 dev.off()
 
+
+postscript("test2.ps",height=10,width=10,horizontal=F)
+sig_vsd = vsd[which(dLRT_res$padj<0.05),]
+colnames(sig_vsd) <- c("CD41-_Ctrl","CD41-_Ctrl","CD41-_Ctrl",
+                      "CD41+_Ctrl","CD41+_Ctrl","CD41+_Ctrl",
+                      "CD41-_Thpo","CD41-_Thpo","CD41-_Thpo",
+                      "CD41+_Thpo","CD41+_Thpo","CD41+_Thpo")
+colors <- colorRampPalette(c("blue","white","red"))(45)
+heatmap.2(sig_vsd,col=colors,scale="row", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+labRow = FALSE,xlab="", ylab="Genes",key.title="Gene expression",cexCol=.8)
+dev.off()
