712cc6c405c890ea31d09961bc63010055dc7418
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-08-24 16:02:44 +0800

    Update modENCODE_tracks.bash

--- modENCODE_tracks.bash
+++ modENCODE_tracks.bash
@@ -17,4 +17,15 @@ more metadata.tsv|cut -f1,7,8,10,12,13|tail -n +2|head -n22
 
 more metadata.tsv|cut -f1|tail -n +2 > file_table.txt
 more metadata.tsv|cut -f7,8,10,12,13|tail -n +2|perl -pe 's/\t/\_/g'|perl -pe 's/\_+/\_/g' > anno_table.txt
+paste file_table.txt anno_table.txt >anno.txt
 
+while read line; do
+   file=$(echo $line | cut -d " " -f 1)
+   caca=$(echo $line | cut -d " " -f 2-999)
+   echo "perl -pe 's/\n/\t$caca\n/g' mm9_mm10/$file.bed > mm10ano/$file.bed"
+done < anno.txt > finalstep.bash
+
+cat * > encode_chip-seq_mm10_unsorted.bed
+sort -k1,1 -k2,2n encode_chip-seq_mm10_unsorted.bed > encode_chip-seq_mm10.bed
+
+~/Downloads/bedToBigBed encode_chip-seq_mm10.bed mm10.chrom.sizes encode_chip-seq_mm10.bb
